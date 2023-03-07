ab04161dca22835d09812785c8c8135368b8bc32
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-06 20:00:53 -0800

    new import added for reading extracting data from a file and using that text data to initialize class MarkovMachine, at the moment the data will only be consoled log to the terminal

--- makeText.js
+++ makeText.js
@@ -1,2 +1,7 @@
-/** Command-line tool to generate Markov text. */
+/** Command-line tool to generate Markov text.
+ * node makeText
+ */
+const { MarkovMachine, getFileData } = require('./markov');
 
+let mrkMch = new MarkovMachine(getFileData('eggs.txt'));
+console.log(mrkMch.makeText(50))
