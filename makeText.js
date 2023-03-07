e85b8fb34d728948aa32f5e52beec965d6bf021d
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-06 20:41:00 -0800

    added init async function to run app, user can now make a call using terminal arguments

--- makeText.js
+++ makeText.js
@@ -2,9 +2,28 @@
  * node makeText
  */
 const { MarkovMachine, getFileData } = require('./markov');
+const axios = require('axios');
 
+const getAxiosData = async () => {
+    try {
+        let {data} = await axios.get(process.argv[3]);
+        console.log(typeof data)
+        return data;
+    } catch (error) {
+        console.log('Failed to get data');
+        process.kill(2);
+    }
+}
 
-if(process.argv[2] === 'file') {
-    let mrkMch = new MarkovMachine( getFileData( process.argv[3] ) );
+let data = '';
+const init = async () => {
+    if(process.argv[2] === 'file') {
+        data = getFileData( process.argv[3] );
+    } else if(process.argv[2] === 'url') {
+        data = await getAxiosData();
+    }
+    let mrkMch = new MarkovMachine( data );
     console.log( mrkMch.makeText() );
 }
+
+init();
