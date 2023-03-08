e88b4d8955a9111591634558fcccbdfb987d51cf
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-03-07 19:14:18 -0800

    test for generating a random number

--- markov.test.js
+++ markov.test.js
@@ -1,2 +1,11 @@
-const { MarkovMachine, getFileData } = require('./markov');
+const { MarkovMachine, getFileData,choice } = require('./markov');
 
+describe('Markov machine test', () => {
+
+    test('random number generated given a number', () => {
+        let num = choice(10);
+        // does it fall between the range of 0 and num
+        expect(num).toBeGreaterThanOrEqual(0);
+        expect(num).toBeLessThanOrEqual(10);
+    })
+})
